8fd4545333f7a22a303b29ee36a365cec54054ea
CraigKnoblauch <craigknoblauch@gmail.com>
2017-11-13 00:25:29 -0700

    Added Student class

---
+++ student.py
@@ -0,0 +1,17 @@
+class Student:
+
+    def __init__(self, fn='', ln='', p=0):
+        self.first_name = fn
+        self.last_name = ln
+        self.pin = p
+        self.seat = '0'  # Because a seat of 0 makes no sense
+
+    def assign_seat(self, seat_str):
+        self.seat = seat_str
+
+    def is_seated(self):
+        return not self.seat == '0'
+
+    # NOTE: Currently just for trouble shooting
+    def print_student(self):
+        print(self.first_name + " " + self.last_name + ": " + self.pin)
