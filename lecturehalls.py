1a713bdedfb5481fd500f88b2a5e33a7186b5df6
CraigKnoblauch <craigknoblauch@gmail.com>
2017-11-16 23:02:26 -0700

    Tweaked and added functions in accordance with issue 3

--- lecturehalls.py
+++ lecturehalls.py
@@ -4,14 +4,20 @@
 
 class Row:
 
-    def __init__(self, let='', sn=[], dn=[], sp=0):
-        self.letter = let
+    def __init__(self, let='', sn=[], dn=[], sp=-9999):
+        self.letter = let.upper()
         self.seat_numbers = sn
         self.assigned_seats = [False] * len(self.seat_numbers)
         self.occupants = [0] * len(self.seat_numbers)  # Will hold Student objects. DO NOT MAKE AN OFF BY ONE ERROR
 
         self.divided_neighbors = dn
-        self.start_position = sp
+
+        # Even if you were counting backwards it seems unlikely that there would
+        # be 9,999 seats in a row. I don't know why I've just got a feeling
+        if sp == -9999:
+            self.start_position = count_seats() % 2
+        else:
+            self.start_position = sp
 
     def add_divided_neighbor(self, sing_dn):
         self.divided_neighbors.append(sing_dn)
@@ -19,19 +25,47 @@ class Row:
     def set_start_position(self, start_i):
         self.start_position = start_i
 
-    def assign_seat(self, seat):
-        self.assigned_seats[seat-1] = True
+    def get_seat_index(self, seat_number):
+        # Assumes that there are no duplicate seat numbers
+        return self.seat_numbers.index(seat_number)
 
-    def free_seat(self, seat):
-        self.assigned_seats[seat-1] = False
+    # NOTE: This may need some testing, not sure if you can pass an object as argument that simply.
+    def assign_seat(self, seat_number, student_obj):
+        self.assigned_seats[self.get_seat_index(seat_number)] = True
+        student_obj.seat = self.letter + str(seat_number)
+        self.occupants[self.get_seat_index(seat_number)] = student_obj
+
+    # TODO: Implementation waiting for a future update
+    def free_seat(self, UNKNOWN):
+        pass
 
     def count_seats(self):
         return len(self.seat_numbers)
+
+    def count_available_seats(self):
+        available_seat_count = self.count_seats()
+
+        for b in self.assigned_seats:
+            if b:
+                available_seat_count -= 1
+
+        return available_seat_count
+
+    # TODO: this needs some testing, but should return the seat numbers of available seats
+    def get_available_seat_list(self):
+        available_seats = []
+        for i in range(0, self.count_seats()):
+            if not self.assigned_seats[i]:
+                open_seat = self.seat_numbers[i]
+                available_seats.append(open_seat)
+
+        return available_seats
 
 
 class NeebHall:
 
     def __init__(self):
+        self.A_ROW = Row(let= 'A', sn= [1,2,3,4,5,6], dn=[3,4])  # NOTE: Should be commented out on Austin-Bren-2017F-83238 branch
         self.B_ROW = Row(let= 'B', sn= list(range(1, 23+1)) )
         self.C_ROW = Row(let= 'C', sn= list(range(1, 26+1)) )
         self.D_ROW = Row(let= 'D', sn= list(range(1, 27+1)) )
