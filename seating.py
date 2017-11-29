4b46a7539c21c54aab30c327a1127c7e4039f7dd
CraigKnoblauch <craigknoblauch@gmail.com>
2017-11-29 16:58:50 -0700

    Linear fill successful

--- seating.py
+++ seating.py
@@ -32,3 +32,25 @@ def parse_file(filename):
     random.shuffle(student_list)
 
     return student_list
+
+# TODO: Modify ot produce output csv file
+def output_file(students):
+    for s in students:
+        s.print_student()
+
+# Process specific to Austin-Bren-2017F-83238, should not be used in full in other versions
+def seat_students(students, hall):
+    N_seats = hall.seat_count
+    N_students = len(students)
+
+    # NOTE: Testing linear fill - SUCCESS
+    # i = 0
+    # for hall_row in hall.HALL.values():
+    #     for seat in hall_row.get_available_seat_list():
+    #         try:
+    #             hall_row.assign_seat(seat, students[i])
+    #         except IndexError:
+    #             break
+    #         i += 1
+
+    output_file(students)
