1cb89dd3cdba5957d37f35d58773d3d827522958
CraigKnoblauch <craigknoblauch@gmail.com>
2017-12-01 17:13:09 -0700

    initial every-other-seat process implemented

--- seating.py
+++ seating.py
@@ -38,10 +38,9 @@ def output_file(students):
     for s in students:
         s.print_student()
 
-# Process specific to Austin-Bren-2017F-83238, should not be used in full in other versions
+# NOTE: Process specific to Austin-Bren-2017F-83238, should not be used in full in other versions
 def seat_students(students, hall):
-    N_seats = hall.seat_count
-    N_students = len(students)
+    student_index = 0
 
     # NOTE: Testing linear fill - SUCCESS
     # i = 0
@@ -52,5 +51,47 @@ def seat_students(students, hall):
     #         except IndexError:
     #             break
     #         i += 1
+
+    # NOTE: Support for divided_neighbors not added because it is not needed in this branches case
+    # First round of seating. Each student is placed with one seat in between them,
+    # starting at the row's start_position
+    # NOTE: This first process may be able to be ported over to master
+    for hall_row in hall.HALL.values():
+        break_after = False
+        pos = hall_row.start_position
+        while True:
+            try:
+                hall_row.assign_seat(hall_row.seat_numbers[pos],
+                                     students[student_index])
+            except IndexError:
+                print("All students seated")
+                break
+
+            student_index += 1
+            pos += 2
+
+            try:
+                if break_after:
+                    break
+                if hall_row.seat_numbers[pos] == hall_row.seat_numbers[-2] or hall_row.seat_numbers[pos] == hall_row.seat_numbers[-1]:
+                    break_after = True
+            except IndexError:
+                print("Index error in seat_students triggered")
+                break
+    # # NOTE
+    # # NOTE: The following lines follow heavy assumptions that ONLY APPLY TO THIS BRANCH. If any of this code is brought to master is should be strongly cherry picked
+    # # NOTE
+    # #
+    # # TODO: Next step, divide remaining students among rows
+    # remaining_student_N = len(students[student_index:]))
+    #
+    # #TODO: Seat these students in a binary fashion
+    # students_per_row = remaining_student_N // hall.count_rows()  #NOTE: Assumes a non zero result as will be the case in this branch ONLY
+    # for hall_row in hall.HALL.values():
+    #     aval_seats = hall_row.get_available_seat_list()
+    #     aval_seats_N = len(aval_seats)
+    #
+    #
+    # remaining_student_N = remaining_student_N % hall.count_rows()
 
     output_file(students)
