0859ba1509fe5e60d7b83d90dc030bbd3903a4ad
CraigKnoblauch <craigknoblauch@gmail.com>
2017-12-03 02:17:43 -0700

    Process may be brought to master; output to be implemented; might want to make seat_students() void

--- seating.py
+++ seating.py
@@ -3,6 +3,7 @@ from student import Student
 import csv
 import random
 import sys
+import math
 
 # NOTE: TODO: Currently no overhead in case the data is input wrong.
 # Assuming, data is input as columns: first, last, pin
@@ -38,66 +39,32 @@ def output_file(students):
     for s in students:
         s.print_student()
 
-# NOTE: Process specific to Austin-Bren-2017F-83238, should not be used in full in other versions
+# NOTE: Process may not be specific to Austin-Bren-2017F-83238
 def seat_students(students, hall):
     updated_students = list()
+    start = True
 
-    # NOTE: Testing linear fill - SUCCESS
-    # i = 0
-    # for hall_row in hall.HALL.values():
-    #     for seat in hall_row.get_available_seat_list():
-    #         try:
-    #             hall_row.assign_seat(seat, students[i])
-    #         except IndexError:
-    #             break
-    #         i += 1
-
-    # NOTE: Support for divided_neighbors not added because it is not needed in this branches case
-    # First round of seating. Each student is placed with one seat in between them,
-    # starting at the row's start_position
-    # NOTE: This first process may be able to be ported over to master
-    for hall_row in hall.HALL.values():
-        break_after = False
-        pos = hall_row.start_position
-        while True:
-            try:
-                students[0]
-            except IndexError:
-                print("All students seated")
-                break
-
-            try:
-                updated_students.append(students[0])
-                hall_row.assign_seat(hall_row.seat_numbers[pos],
-                                     students.pop(0))
-            except IndexError:
-                print("seat_numbers index error")
-                break
-
-            pos += 2
-
-            try:
-                if break_after:
+    while len(students) != 0:
+        D = len(students) / hall.count_available_seats()  # Density of students per seats
+        for hall_row in hall.HALL.values():
+            if start:
+                i = hall_row.start_position
+            else:
+                i = 0
+            r = D * hall_row.count_available_seats()  # Get the number of students to be seated in this row
+            d = hall_row.count_available_seats() / r  # Get the number of seats per students
+            aval_seats = hall_row.get_available_seat_list()
+            while i < hall_row.count_available_seats():
+                try:
+                    updated_students.append(students[0])
+                    hall_row.assign_seat(aval_seats[math.ceil(i)], students.pop(0))  
+                except IndexError:
+                    print("All students seated")
                     break
-                if hall_row.seat_numbers[pos] == hall_row.seat_numbers[-2] or hall_row.seat_numbers[pos] == hall_row.seat_numbers[-1]:
-                    break_after = True
-            except IndexError:
-                print("Index error in seat_students triggered")
-                break
-    # # NOTE
-    # # NOTE: The following lines follow heavy assumptions that ONLY APPLY TO THIS BRANCH. If any of this code is brought to master is should be strongly cherry picked
-    # # NOTE
-    # #
-    # # TODO: Next step, divide remaining students among rows
-    # remaining_student_N = len(students[student_index:]))
-    #
-    # #TODO: Seat these students in a binary fashion
-    # students_per_row = remaining_student_N // hall.count_rows()  #NOTE: Assumes a non zero result as will be the case in this branch ONLY
-    # for hall_row in hall.HALL.values():
-    #     aval_seats = hall_row.get_available_seat_list()
-    #     aval_seats_N = len(aval_seats)
-    #
-    #
-    # remaining_student_N = remaining_student_N % hall.count_rows()
+                print("d: " + str(d))
+                i += d
+                print("i: " + str(i))
+
+        start = False
 
     output_file(updated_students)
